0da9c29eadb4ef632c28a9ab396c3078d610b1f1
William Moreno <graficowm@gmail.co>
2023-10-22 13:56:14 -0500

    Base ajuste nuevo desarrollo

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+const {task, watch, src, dest} = require('gulp');
+const pug = require('gulp-pug');
+const sass = require('gulp-sass')(require('sass'));
+const autoprefixer = require('gulp-autoprefixer');
+const imagemin = require('gulp-imagemin');
+
+task('views', () => {
+  return src('./src/pages/*.pug')
+    .pipe(pug({pretty: true,}))
+    .pipe(dest('./dist'));
+});
+
+task('styles', () => {
+    return src('./src/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(dest('./dist/css'));
+});
+
+task('images', () => {
+    return src('./src/img/*')
+    .pipe(imagemin())
+    .pipe(dest('./dist/img'));
+});
+
+task('default', () => {
+    watch('./src/**/*.pug', task('views'));
+    watch('./src/sass/**/*.scss', task('styles'));
+});
+
+
+
