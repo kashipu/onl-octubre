11ecddbaf6a9a700af8c8f78d2189e0a65d36406
William Moreno <graficowm@gmail.co>
2023-10-25 21:37:41 -0500

    Slider y ajustes de responsive

--- dist/js/main.js
+++ dist/js/main.js
@@ -11,3 +11,31 @@ videos.forEach(video => {
     video.currentTime = 0;
   });
 });
+
+// Slider
+$('.slider-capsulas').slick({
+    dots: true,
+    infinite: false,
+    speed: 300,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
